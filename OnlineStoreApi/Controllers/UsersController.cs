16b0d2be80fdae5a83dfba0a86804b0dfc473838
ahmads1990 <ahmads20002000@gmail.com>
2024-03-19 10:27:18 +0200

    Add users controller

---
+++ OnlineStoreApi/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStoreApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterAsync(RegisterModel registerModel)
+        {
+            var result = await _userService.RegisterUserAsync(registerModel);
+
+            if (!result.IsAuthenticated)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginAsync(LoginModel loginModel)
+        {
+            var result = await _userService.LoginUserAsync(loginModel);
+
+            if (!result.IsAuthenticated)
+                return BadRequest(result.Message);
+
+            //Todo send confirmation mail
+            return Ok(result);
+        }
+    }
+}
