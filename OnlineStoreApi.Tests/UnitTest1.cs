4bc0c753aa030572a87d297441f0c379e426b0a4
Ahmad <ahmads20002000@gmail.com>
2024-03-19 05:50:55 +0200

    Create tests Nunit project

---
+++ OnlineStoreApi.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OnlineStoreApi.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
