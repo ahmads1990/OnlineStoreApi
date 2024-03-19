abfcd733d1bdb706c9dfea5ea1ea19064c12dfdb
Ahmad <ahmads20002000@gmail.com>
2024-03-19 06:29:52 +0200

    Add product dtos

---
+++ OnlineStoreApi/Models/Dtos/AddNewProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineStoreApi.Models.Dtos
+{
+    public class AddNewProductDto
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public IFormFile ProductImage { get; set; }
+        public float Price { get; set; }
+        public int MinimumQuantity { get; set; } = 1;
+        public float DiscountRate { get; set; }
+    }
+}
---
+++ OnlineStoreApi/Models/Dtos/UpdateProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineStoreApi.Models.Dtos
+{
+    public class UpdateProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public IFormFile ProductImage { get; set; }
+        public float Price { get; set; }
+        public int MinimumQuantity { get; set; } = 1;
+        public float DiscountRate { get; set; }
+    }
+}
