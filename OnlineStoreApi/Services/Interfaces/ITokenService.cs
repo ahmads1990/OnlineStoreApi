c72e09ca128fd13fbefdcb6d39a1b22592515426
Ahmad <ahmads20002000@gmail.com>
2024-03-20 05:27:29 +0200

    Create ITokenService inteface for jwt operations

---
+++ OnlineStoreApi/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace OnlineStoreApi.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateJwtTokenAsync(ApplicationUser user);
+        Task<AuthModel> RefreshAccessTokenAsync(string refreshToken);
+    }
+}
